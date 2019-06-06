994fb7c243964e10cc16ca8b7fde58d155689404
archezi <luka.kaczmarek@gmail.com>
2019-06-06 09:19:19 +0100

    finishing desktrop version

---
+++ .history/dist/js/main_20190604121426.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+
+  // Scroll Events
+  $(window).scroll(function(){
+    var wScroll = $(this).scrollTop();
+  
+    // Activate menu
+    if (wScroll > 20) {
+      $('#main_navigation').addClass('active');
+      // $('#slide_out_menu').addClass('scrolled');
+    }
+    else {
+      $('#main_navigation').removeClass('active');
+      // $('#slide_out_menu').removeClass('scrolled');
+    };
+  
+  
+    //Scroll Effects
+  
+  });
+
+  wow = new WOW(
+    {
+      boxClass:     'wow',      // default
+      animateClass: 'animated', // default
+      offset:       0,          // default
+      mobile:       true,       // default
+      live:         true        // default
+    }
+  )
+  wow.init();
+
+}
